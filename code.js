9272ff671ef919ac27e02f27d27f3c01a0af8f6a
Beatriz Cerqueira <beatrizcerqueira008@gmail.com>
2025-09-10 08:21:08 -0300

    Passar valores

---
+++ code.js
@@ -0,0 +1,4 @@
+let qtdNumerosASortear = parseFloat(prompt('Diga quantos números serão sorteados: '));
+let inicioIntervalo = parseFloat(prompt('Diga a partir de qual número será gerado os números aleatórios: '));
+let fimIntervalo = parseFloat(prompt('Diga qual será o fim desse intervalo'));
+
