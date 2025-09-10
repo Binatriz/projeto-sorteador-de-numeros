2ed8e466ae42986388a0b0c6fd853381669efeb9
Beatriz Cerqueira <beatrizcerqueira008@gmail.com>
2025-09-10 08:32:25 -0300

    Sortear numeros

--- code.js
+++ code.js
@@ -1,4 +1,15 @@
 let qtdNumerosASortear = parseFloat(prompt('Diga quantos números serão sorteados: '));
 let inicioIntervalo = parseFloat(prompt('Diga a partir de qual número será gerado os números aleatórios: '));
 let fimIntervalo = parseFloat(prompt('Diga qual será o fim desse intervalo'));
+let resultado;
+
+let listaNumerosSorteados = [];
+
+for (i = 0; i < qtdNumerosASortear; i++) {
+    resultado = Math.floor(Math.random() * fimIntervalo + inicioIntervalo);
+    console.log(resultado);
+    listaNumerosSorteados[i] = resultado;
+}
+console.log(listaNumerosSorteados);
+alert(`Resultado do Valor é ${listaNumerosSorteados}`);
 
