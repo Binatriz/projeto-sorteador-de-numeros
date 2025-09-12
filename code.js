d7b3cc251bd9e6f1f440ed41e7f6135df353d2c2
Beatriz Cerqueira <beatrizcerqueira008@gmail.com>
2025-09-12 10:47:52 -0300

    adicionar input e botão

--- code.js
+++ code.js
@@ -2,27 +2,27 @@ let resultado;
 
 function sortear() {
 
-    let quantidade = parseInt(document.getElementById("quantidade"));
-    let de = parseFloat(document.getElementById("de"));
-    let ate = parseFloat(document.getElementById("ate"));
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseFloat(document.getElementById("de").value);
+    let ate = parseFloat(document.getElementById("ate").value);
 
     function verificar_Resposta(verificar_Variavel, pergunta_Usuario) {
-        let respostaUsuario = verificar_Variavel(prompt(pergunta_Usuario));
+        let respostaUsuario = verificar_Variavel(pergunta_Usuario);
         while (isNaN(respostaUsuario) || respostaUsuario <= 0) {
-            respostaUsuario = verificar_Variavel(prompt(pergunta_Usuario + '\n Aceita-se somente números e que sejam maiores que 0'));
+            respostaUsuario = verificar_Variavel(pergunta_Usuario + '\n Aceita-se somente números e que sejam maiores que 0');
         }
         return respostaUsuario;
     }
-    let qtdNumerosASortear = verificar_Resposta(parseInt, 'Diga quantos números serão sorteados: ');
-    let inicioIntervalo = verificar_Resposta(parseFloat, 'Diga a partir de qual número será gerado os números aleatórios: ');
-    let fimIntervalo = verificar_Resposta(parseFloat, 'Diga qual será o fim desse intervalo');
+
+    let qtdNumerosASortear = verificar_Resposta(parseInt, quantidade);
+    let inicioIntervalo = verificar_Resposta(parseFloat, de);
+    let fimIntervalo = verificar_Resposta(parseFloat, ate);
 
     let listaNumerosSorteados = [];
 
     while (fimIntervalo < inicioIntervalo) {
-        fimIntervalo = verificar_Resposta(parseFloat, 'Diga qual será o fim desse intervalo');
+        fimIntervalo = verificar_Resposta(parseFloat, ate);
     }
-    console.log(`A quantidade de: ${qtdNumerosASortear}, o inicio do intervalo é: ${inicioIntervalo} e o fim do intervalo é: ${fimIntervalo}`);
     for (i = 0; i < qtdNumerosASortear; i++) {
         resultado = Math.floor(Math.random() * (fimIntervalo - inicioIntervalo) + 1) + inicioIntervalo;
         listaNumerosSorteados[i] = resultado;
@@ -33,6 +33,6 @@ function sortear() {
     for (i = 0; i < listaNumerosSorteados.length; i++) {
         numerosSorteados += ('<br>' + listaNumerosSorteados[i] + '<br>');
     }
-    console.log(`2 - A quantidade de: ${qtdNumerosASortear}, o inicio do intervalo é: ${inicioIntervalo} e o fim do intervalo é: ${fimIntervalo}`);
-    document.write(`Resultado do sorteio é: ${numerosSorteados}`);
+    let resposta = document.getElementById("resultado");
+    resposta.innerHTML = (`Resultado do sorteio é: ${numerosSorteados}`);
 }
